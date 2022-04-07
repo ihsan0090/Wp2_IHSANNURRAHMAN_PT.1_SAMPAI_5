aa03ca3467a67a373199382bb8d9dc199c215248
ihsan <haaseeggaw4@gmail.com>
2022-04-07 12:19:35 +0700

    pembuatan tampilan about Rentalbuku,setting autoload,& pembuatan css

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -14,9 +14,9 @@
                 </hgroup> 
                 <nav> 
                     <ul> 
-                        <li><a href="<?php echo base_url().'index.php/web' ?>">Home</a>
+                        <li><a href="<?php echo base_url().'Web' ?>">Home</a>
                     </li> 
-                    <li><a href="<?php echo base_url().'index.php/web/about' ?>">About</a></li> 
+                    <li><a href="<?php echo base_url().'Web/about' ?>">About</a></li> 
                 </ul> 
             </nav> 
             <div class="clear"></div> 
